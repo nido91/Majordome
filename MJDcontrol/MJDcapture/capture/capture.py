f5148fa123741ab24ab0d1173cf410120dfb8d36
nido91 <denisdoisos@gmail.com>
2016-03-13 20:15:06 +0100

    Update capture.py

--- MJDcontrol/MJDcapture/capture/capture.py
+++ MJDcontrol/MJDcapture/capture/capture.py
@@ -8,7 +8,7 @@ with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     time.sleep(0.5)
     i=0
-    for capture in camera.capture_continuous('img{timestamp:%Y-%m-%d-%H-%M-%S}.jpg'):
+    for capture in camera.capture_continuous('img{counter:02d}-{timestamp:%Y-%m-%d-%H-%M}.jpg'):
 #        print('Captured %s' % capture)
         time.sleep(0.5) # wait x sec
         i=i+1
